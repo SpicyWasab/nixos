c7d98552b9ee992e52320f24f1d75037773e56b1
wasab <le_wasabi_ca_pik@proton.me>
2025-05-31 02:08:17 +0200

    moved hyprland configuration

--- home.nix
+++ home.nix
@@ -1,14 +1,6 @@
 { config, pkgs, inputs, system, lib, ... }:
 
 {
-  # imports = [inputs.walker.homeManagerModules.default];
-  
-  # imports = [
-    # inputs.zen-browser.homeModules.twilight
-    # or inputs.zen-browser.homeModules.twilight
-    # or inputs.zen-browser.homeModules.twilight-official
-  # ];
-
   home = {
     username = "wasab";
     homeDirectory = "/home/wasab";
@@ -23,23 +15,18 @@
       xz
       bat
       htop
-      git # use a module later
+      # git # use a module later
       nodejs # my favourite scripting language ngl
 
-      # gemini
-      # av-98 (apparently broken)
-      asuka
+      # terminal browsers
       offpunk
-
-      # that's so cool
-      # browsh
-      # firefox # unfortunately it's a dependancy
+      w3m
+      lynx
 
       # hyprland-related
       hypridle
       hyprpaper
       waybar
-      # xdg-desktop-portal-gtk # for some reason idk (https://discourse.nixos.org/t/xdg-portals-all-broken/48308/22)
 
       # gui apps (gnome core)
       nautilus
@@ -51,7 +38,6 @@
       loupe
       evince
       warp
-      # geary
       
       # slowly replacing every gui app with an alternative
       zathura # instead of evince (which was great)
@@ -74,7 +60,7 @@
       obsidian
 
       # ocaml !
-      ocamlPackages.utop # vive la MP2I
+      ocamlPackages.utop # vive la mp2i
 
       # for scientific calculation (vive la prépa)
       octave
@@ -95,30 +81,24 @@
   # enabling user fonts
   fonts.fontconfig.enable = true;
 
-  # gtk cursor
-  # gtk.enable = true;
-  # x11.enable = true;
-  
   stylix.enable = true;
-  # stylix.image = "$HOME/Wallpapers/a_computer_room_with_a_desk_and_a_computer_monitor.jpg";
+  # stylix.image = "$home/wallpapers/a_computer_room_with_a_desk_and_a_computer_monitor.jpg";
   # blueforest looked good as well
   # tried spaceduck
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
-  stylix.cursor.package = pkgs.bibata-cursors;
-  stylix.cursor.name = "Bibata-Modern-Ice";
-  stylix.cursor.size = 24;
-  stylix.opacity.applications = 0.6;
-  stylix.opacity.terminal = 0.6;
-  # home.pointerCursor = {
-    # gtk.enable = true;
-    # x11.enable = true;
-    # hyprcursor.enable = true;
-    # package=pkgs.bibata-cursors;
-    # name="Bibata-Modern-Ice";
-    # size=24;
-  # };
+  stylix = {
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
+    cursor = {
+      package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+        size = 24;
+    };
+    opacity = {
+      applications = 0.6;
+      terminal = 0.6;
+    };
+  };
   
-  # usual XDG user dirs
+  # usual xdg user dirs
   xdg = {
     enable = true;
     userDirs = {
@@ -133,12 +113,6 @@
       pictures = "$HOME/Images";
       publicShare = "$HOME/Public";
     };
-    # mimeApps = {
-      # enable = true;
-      # defaultApplications = {
-      #   "document/pdf" = "zathura.pdf";
-      # };
-    # };
     portal = {
       enable = true;
       xdgOpenUsePortal = true;
@@ -167,17 +141,19 @@
   };
   
   # my favorite shell
-  # (because I'm too lazy and don't have time to get into zsh for now)
+  # (because i'm too lazy and don't have time to get into zsh for now)
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       set fish_greeting
-      set -g EDITOR nvim
+      set -g editor nvim
+
+      alias ls="eza"
 
       function y
-	      set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	      set tmp (mktemp -t "yazi-cwd.xxxxxx")
 	      yazi $argv --cwd-file="$tmp"
-	      if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+	      if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$pwd" ]
 		      builtin cd -- "$cwd"
 	      end
 	      rm -f -- "$tmp"
@@ -189,6 +165,13 @@
     ];
     
   };
+
+  # programs.starship = {
+  #   enable = true;
+  #   settings = {
+  #     
+  #   };
+  # };
   
   # neovim !
   programs.neovim = {
@@ -202,12 +185,191 @@
   # git (took me a while)
   programs.git = {
     enable = true;
-    userName  = "Wasab'";
+    userName  = "wasab'";
     userEmail = "le_wasabi_ca_pik@proton.me";
   };
 
   # everything hyprland related
-  # programs.hyprland.enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = [ "eDP-1, 1536x1024@59.99700,0x0,1" ];
+      
+      # programs
+      "$terminal" = "kitty";
+      "$filemanager" = "nautilus";
+      "$menu" = "tofi-drun | xargs hyprctl dispatch exec --";
+      "$browser" = "zen";
+
+      exec-once = [
+        "waybar"
+        "hyprpaper"
+        "hypridle"
+      ];
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        # probably already defined by stylix
+        # "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        # "col.inactive_border" = "rgba(595959aa)";
+        resize_on_border = true;
+        layout = "dwindle"; # what is this, again ?
+      };
+
+      decoration = {
+        rounding = 10;
+
+        shadow = {
+          enabled = true;
+          range = 4;
+          render_power = 3;
+          # color = "rgba(1a1a1aee)";
+        };
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          vibrancy = 0.1696;
+        };
+      };
+
+      animations = {
+        enabled = true;
+
+        bezier = [
+          "easeOutQuint,0.23,1,0.32,1"
+          "easeInOutCubic,0.65,0.05,0.36,1"
+          "linear,0,0,1,1"
+          "almostLinear,0.5,0.5,0.75,1.0"
+          "quick,0.15,0,0.1,1"
+        ];
+
+        animation = [
+          "global, 1, 10, default"
+          "border, 1, 5.39, easeOutQuint"
+          "windows, 1, 4.79, easeOutQuint"
+          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+          "windowsOut, 1, 1.49, linear, popin 87%"
+          "fadeIn, 1, 1.73, almostLinear"
+          "fadeOut, 1, 1.46, almostLinear"
+          "fade, 1, 3.03, quick"
+          "layers, 1, 3.81, easeOutQuint"
+          "layersIn, 1, 4, easeOutQuint, fade"
+          "layersOut, 1, 1.5, linear, fade"
+          "fadeLayersIn, 1, 1.79, almostLinear"
+          "fadeLayersOut, 1, 1.39, almostLinear"
+          "workspaces, 1, 1.94, almostLinear, fade"
+          "workspacesIn, 1, 1.21, almostLinear, fade"
+          "workspacesOut, 1, 1.94, almostLinear, fade"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      master = {
+        new_status = "master";
+      };
+
+      input = {
+        kb_layout = "fr";
+        kb_variant = "oss";
+        follow_mouse = 1;
+        
+        touchpad = {
+          natural_scroll = true;
+        };
+      };
+
+      gestures = {
+        workspace_swipe = true;
+      };
+
+      "$mainMod" = "super";
+
+      bind = [
+        "$mainMod, r, exec, pkill waybar && hyprctl dispatch exec waybar"
+        "$mainMod, t, exec, $terminal"
+        "$mainMod, q, killactive,"
+        "$mainMod, m, exit,"
+        "$mainMod, e, exec, $filemanager"
+        "$mainMod, v, togglefloating,"
+        "$mainMod, space, exec, $menu"
+        "$mainMod, p, pseudo," # dwindle"
+        "$mainMod, j, togglesplit," # dwindle
+        "$mainMod, b, exec, $browser"
+        
+        # move focus with arrow keys
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        "$mainMod, ampersand, workspace, 1"
+        "$mainMod, eacute, workspace, 2"
+        "$mainMod, quotedbl, workspace, 3"
+        "$mainMod, apostrophe, workspace, 4"
+        "$mainMod, parenleft, workspace, 5"
+
+        "$mainMod SHIFT, ampersand, movetoworkspace, 1"
+        "$mainMod SHIFT, eacute, movetoworkspace, 2"
+        "$mainMod SHIFT, quotedbl, movetoworkspace, 3"
+        "$mainMod SHIFT, apostrophe, movetoworkspace, 4"
+        "$mainMod SHIFT, parenleft, movetoworkspace, 5"
+
+        "$mainMod, S, togglespecialworkspace, magic"
+        "$mainMod SHIFT, S, movetoworkspace, special:magic"
+
+        "$mainMod, return, submap, resize"
+      ];
+
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl --exponent=1.5 --min-value=1% -- set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl --exponent=1.5 --min-value=1% -- set 5%-"
+      ];
+
+      bindl = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl --exponent=1.5 --min-value=1% -- set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl --exponent=1.5 --min-value=1% -- set 5%-"
+      ];
+
+      windowrulev2 = [
+        "suppressevent maximize, class:.*"
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+      ];
+    };
+
+    extraConfig = "
+      submap = resize
+
+      binde = , right, resizeactive, 40 0
+      binde = , left, resizeactive, -40 0
+      binde = , up, resizeactive, 0 -40
+      binde = , down, resizeactive, 0 40
+
+      bind = , escape, submap, reset
+
+      submap = reset
+    ";
+  };
 
   programs.hyprlock = {
     enable = true;
@@ -215,15 +377,11 @@
 
   services.hypridle.enable = true;
   services.hyprpaper.enable = true;
-  
-  # bluetooth
-  # services.blueman.enable = true;
 
   # programs.waybar.enable = true;
   programs.tofi = {
     enable = true;
     settings = {
-      # background-color = "#000000";
       border-width = 0;
       # font = "monospace";
       height = "100%";
@@ -247,51 +405,6 @@
   # KDE connect
   services.kdeconnect.enable = true;
   
-  # for nautilus
-  # services.gvfs.enable = true;
-
-
-  # programs.walker = {
-  #   enable = true;
-  #   runAsService = true;
-
-  #   # All options from the config.json can be used here.
-  #   config = {
-  #     search.placeholder = "Example";
-  #     ui.fullscreen = true;
-  #     list = {
-  #       height = 200;
-  #     };
-  #     websearch.prefix = "?";
-  #     switcher.prefix = "/";
-  #   };
-
-  #   # If this is not set the default styling is used.
-  #   # style = ''
-  #   #   * {
-  #   #     color: #dcd7ba;
-  #   #    }
-  #   #  '';
-  # };
-
-  # networking.firewall = rec {
-  #   allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-  #   allowedUDPPortRanges = allowedTCPPortRanges;
-  # };
-  
-  # programs.zen-browser.enable = true;
-
-  # programs.obsidian.enable = true;
-  # programs.nautilus.enable = true;
-  # programs.baobab.enable = true; # apparently does not exists
-  # programs.gnome-disk-utility.enable = true;
-  # programs.mission-center.enable = true;
-  # programs.sushi.enable = true;
-  # programs.decibels.enable = true; # same issue
-  # programs.loupe.enable = true;
-  # programs.evince.enable = true; # same
-
-
   # let home manager install and manage itself
   programs.home-manager.enable = true;
 }
