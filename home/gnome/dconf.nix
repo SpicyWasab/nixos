271c4ed9f7ac92f34bcdc1a8b02b60ab68c7254e
Wasab <spicywasab+github@proton.me>
2026-01-10 17:38:39 +0100

    feat: tried to set an option to disable hot corner

--- home/gnome/dconf.nix
+++ home/gnome/dconf.nix
@@ -2,12 +2,16 @@
 
 {
   dconf.settings = {
+    "/org/gnome/desktop/interface" = {
+      enable-hot-corners = false;
+    };
+
     "org/gnome/shell/extensions" = {
       enabled-extensions = [
         "blur-my-shell@aunetx"
       ];
 
-      "org/gnome/shell/extensions/blur-my-shell/panel/blue" = false;
+      "org/gnome/shell/extensions/blur-my-shell/panel/blur" = false;
     };
   };
 }
