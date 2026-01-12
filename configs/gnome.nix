5a28738c8b9a29ae7525fe39ebce12494c6f6ca9
Wasab <spicywasab+github@proton.me>
2026-01-12 19:21:09 +0100

    feat: added fwupd in order to get GNOME security tab to work

--- configs/gnome.nix
+++ configs/gnome.nix
@@ -9,4 +9,5 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
   services.flatpak.enable = true;
+  services.fwupd.enable = true; # trying to get GNOME security tab to work
 }
